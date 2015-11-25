f76640541069b9a13d13ad51cee0c27a099db371
Leonid Melkozerov <leonid,melkozerov13@imperial.ac.uk>
2015-11-25 21:08:30 +0000

    added basic registers and instruction classes.

---
+++ src/compiler/instructionVisit/Instruction.java
@@ -0,0 +1,13 @@
+package instructionVisit;
+
+public class Instruction {
+	private String operator;
+	private String arg1;
+	private String arg2;
+ 
+	public Instruction(String operator, String arg1, String arg2) {
+	  this.operator = operator;
+	  this.arg1 = arg1;
+      this.arg2 = arg2;
+	}
+}
