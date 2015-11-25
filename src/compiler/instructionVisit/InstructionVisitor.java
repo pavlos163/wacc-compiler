43dc56ccf59eafc4cfb230adfc8506bd71fbaf97
Leonid Melkozerov <leonid,melkozerov13@imperial.ac.uk>
2015-11-25 21:13:41 +0000

    style fix

--- src/compiler/instructionVisit/InstructionVisitor.java
+++ src/compiler/instructionVisit/InstructionVisitor.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InstructionVisitor {
-	private List<Instruction> output;
-	private List<Register> registers;
+  private List<Instruction> output;
+  private List<Register> registers;
   private Register r0;
   private Register r1;
   private Register r2;
@@ -15,8 +15,8 @@ public class InstructionVisitor {
   private Register pc;
 
   public InstructionVisitor() {
-	  output = new ArrayList<Instruction>();
-	  registers = new ArrayList<Register>();
+    output = new ArrayList<Instruction>();
+	registers = new ArrayList<Register>();
 	  
   }
 }
--- src/compiler/instructionVisit/Register.java
+++ src/compiler/instructionVisit/Register.java
@@ -1,17 +1,16 @@
 package instructionVisit;
 
 public class Register {
-	private String name;
-	private boolean isFree;
-	public Register(String name){
-		this.name = name;
-		this.isFree = true;
+  private String name;
+  private boolean isFree;
+  public Register(String name){
+    this.name = name;
+    this.isFree = true;
 	}
-public String getName(){
-	return this.name;
- }
-public boolean isFree(){
-	return this.isFree;
- }
-
+  public String getName(){
+    return this.name;
+  }
+  public boolean isFree(){
+    return this.isFree;
+  }
 }
